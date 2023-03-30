e7c8068e664d1049aa5fcb170cd7ec1f7f892379
Глеб Чекмезов <glebchekmezov@MacBook-Pro-Gleb.local>
2023-03-30 16:02:41 +0300

    new files

--- sample-pytest/conftest.py
+++ sample-pytest/conftest.py
@@ -2,6 +2,8 @@ import os
 
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture(scope="function")
@@ -15,10 +17,11 @@ def driver():
     options.set_capability("enableVideo", "true")
     ############################################################################################
 
-    driver = webdriver.Remote(
-        command_executor=zebrunner_selenium_grid,
-        options=options
-    )
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    # driver = webdriver.Remote(
+    #     command_executor=zebrunner_selenium_grid,
+    #     options=options
+    # )
 
     driver.implicitly_wait(10)
     driver.maximize_window()
