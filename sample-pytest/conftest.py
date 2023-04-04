d88607ab900ed7b6a46d178ceae0423d41cd272e
hchekmezov <hchekmezov@solvd.com>
2023-04-04 10:32:08 +0300

    updates

--- sample-pytest/conftest.py
+++ sample-pytest/conftest.py
@@ -2,8 +2,8 @@ import os
 
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture(scope="function")
@@ -11,17 +11,17 @@ def driver():
     zebrunner_selenium_grid = os.getenv("ZEBRUNNER_SELENIUM_GRID", "http://127.0.0.1:4444")
 
     ############################################################################################
-    options = webdriver.ChromeOptions()
+    options = webdriver.FirefoxOptions()
     options.platform_name = "linux"
     options.browser_version = "109.0"
     options.set_capability("enableVideo", "true")
     ############################################################################################
 
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    # driver = webdriver.Remote(
-    #     command_executor=zebrunner_selenium_grid,
-    #     options=options
-    # )
+    # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver = webdriver.Remote(
+        command_executor=zebrunner_selenium_grid,
+        options=options
+    )
 
     driver.implicitly_wait(10)
     driver.maximize_window()
